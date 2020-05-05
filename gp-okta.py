3949d4d4c062171a5f310b515a227a2755c1a3d2
Oleksandr Pryimak <sasha@thumbtack.com>
2020-05-05 15:24:09 -0700

    Fix a priority for push notification

--- gp-okta.py
+++ gp-okta.py
@@ -143,7 +143,7 @@ def load_conf(cf):
 def mfa_priority(conf, ftype, fprovider):
 	if ftype == 'token:software:totp':
 		ftype = 'totp'
-	if ftype not in ['totp', 'sms']:
+	if ftype not in ['totp', 'sms', 'push']:
 		return 0
 	mfa_order = conf.get('mfa_order', '')
 	if ftype in mfa_order:
